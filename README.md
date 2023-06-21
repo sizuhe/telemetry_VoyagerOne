22e628cb0a1263795ae7503b8dae96e3d37ff1b7
Simon Zuluaga <simon.zuluagah@udea.edu.co>
2023-06-21 16:46:13 -0500

    Receiver update

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 These codes describe how our CanSat *VoyagerOne* electronics works. Made for the Latin American Space Challenge (*LASC*) competition.
 
-This project is being developed in University of Antioquia by *Voyager* student group.
-
-![Alt text](https://grupoastra.github.io/icons/voyager.png)
-
-Link: https://grupoastra.github.io/voyager/
+This project is being developed in University of Antioquia by [*Voyager*](https://grupoastra.github.io/voyager/) student group.
---
+++ main/receiver/receiver.ino
@@ -0,0 +1,47 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+/* ----- Connections -----
+| Arduino UNO | LoRa SX1278 |
+|-------------|-------------|
+| GND         | GND         |
+| 3.3V        | 3.3V        |
+| RST         | RST         |
+| DI07        | DI00        |
+| DI10        | NSS         |
+| DI11        | MOSI        |
+| DI12        | MISO        |
+| DI13        | SCK         |
+*/
+
+// ----- LORA SETTINGS ------
+#define LORA_FREQ 433E6     // LoRa frequency band
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!LoRa.begin(LORA_FREQ)) {
+    Serial.println("LoRa init failed");
+    while (1);
+  }
+}
+
+
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    int i = 0;
+    char dataPacket[255];  // Buffer to hold data packet
+    while (LoRa.available()) {
+      dataPacket[i++] = (char)LoRa.read();    // Adds every character to a data array
+      if (i >= sizeof(dataPacket)) {
+        break;
+      }
+    }
+    dataPacket[i] = '\0';  // End of dataPacket array
+    Serial.println(dataPacket);
+  }
+}
